645b3b4734feea5d47f507ac4124d5eb1b6284ec
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-11 03:35:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-cell = "   "
-row = cell + "|" + cell + "|"
-board = row * 3 * 3
-puts board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
 
 display_board
